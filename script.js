63567c1635a81c9de3d1a8d101cd1e6586c7f403
Devika Sujith <devikasujith12@gmail.com>
2022-03-31 14:06:46 +0400

    init js variables

---
+++ script.js
@@ -0,0 +1,7 @@
+const textBill = document.querySelector('#bill-amt')
+const textNoOfPeople = document.querySelector('#no_of_ppl')
+
+const resultTip = document.querySelector('.result_amt--tip')
+const resultTotal = document.querySelector('.result_amt--total')
+
+const btnTip = document.querySelectorAll('.btn--tip')
