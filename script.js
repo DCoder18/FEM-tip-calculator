0f36729bb6db7847fbfe6dcaee9c963e20c07db6
Devika Sujith <devikasujith12@gmail.com>
2022-03-31 14:07:33 +0400

    Establish logic workflow

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ const resultTip = document.querySelector('.result_amt--tip')
 const resultTotal = document.querySelector('.result_amt--total')
 
 const btnTip = document.querySelectorAll('.btn--tip')
+
+/* 
+1. Get bill amount
+2. Get selected tip amount
+3. Get number of people
+4. Calculate total 
+    Total Tip: bill*(tip/100)
+    Total Bill: bill + tip
+
+    Tip / person: Total tip/no of ppl
+    Total /person: Total/no of ppl
+*/
