1b62c429bb119da16becfe4e39a550131cae4697
Devika Sujith <devikasujith12@gmail.com>
2022-03-31 17:33:54 +0400

    Script (WIP)

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
-const textBill = document.querySelector('#bill-amt')
-const textNoOfPeople = document.querySelector('#no_of_ppl')
+const elBill = document.querySelector('#bill-amt')
+const elNoOfPeople = document.querySelector('#no_of_ppl')
 
-const resultTip = document.querySelector('.result_amt--tip')
-const resultTotal = document.querySelector('.result_amt--total')
+const elResultTip = document.querySelector('.result_amt--tip')
+const elResultTotal = document.querySelector('.result_amt--total')
 
-const btnTip = document.querySelectorAll('.btn--tip')
+const elBtnTip = document.querySelectorAll('.btn--tip')
+const elTipCustom = document.querySelector('#tip-custom')
+
+let bill = 0;
+let no_of_ppl = 0;
+let tip_custom = 0;
+let total_amount = 0;
+
+for (const btn of elBtnTip) {
+  btn.addEventListener('click', function() {
+    bill = Number(elBill.value)
+    no_of_ppl = Number(elNoOfPeople.value)
+    tip = Number(btn.getAttribute('data-tip-amount')/100)
+    // tip_custom = isset(elTipCustom.value)
+
+    total_amount = bill + tip
+
+    console.log(bill, no_of_ppl, tip, total_amount);
+  })
+}
+
 
 /* 
 1. Get bill amount
