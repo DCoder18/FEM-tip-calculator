f5bee6e356beae8eb0a40848ee7051cc4402c333
Devika Sujith <devikasujith12@gmail.com>
2022-04-08 11:04:27 +0400

    Update script.js

--- script.js
+++ script.js
@@ -5,25 +5,32 @@ const elResultTip = document.querySelector('.result_amt--tip')
 const elResultTotal = document.querySelector('.result_amt--total')
 
 const elBtnTip = document.querySelectorAll('.btn--tip')
-const elTipCustom = document.querySelector('#tip-custom')
+const elTipCustom = document.querySelector('#custom-tip')
 
 let bill = 0;
 let no_of_ppl = 0;
+let tip = 0;
 let tip_custom = 0;
-let total_amount = 0;
+// const total_amount = 0;
+// const total_tip = 0;
+
+//Get total tip
+const total_tip = (bill, tip) => bill * (tip/100);
 
 for (const btn of elBtnTip) {
   btn.addEventListener('click', function() {
     bill = Number(elBill.value)
     no_of_ppl = Number(elNoOfPeople.value)
-    tip = Number(btn.getAttribute('data-tip-amount')/100)
-    // tip_custom = isset(elTipCustom.value)
+    tip = Number(btn.getAttribute('data-tip-amount'))
+    tip_custom = elTipCustom.value
 
-    total_amount = bill + tip
+    total_tip(bill, tip);
 
-    console.log(bill, no_of_ppl, tip, total_amount);
+    console.log(bill, no_of_ppl, tip, total_tip);
   })
 }
+
+
 
 
 /* 
