b75e75421f496556928fa7e5a5ccc54bc6e340ba
Devika Sujith <devikasujith12@gmail.com>
2022-04-18 14:01:23 +0400

    Calculate tip and amount

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const elBill = document.querySelector('#bill-amt')
 const elNoOfPeople = document.querySelector('#no_of_ppl')
+const elErrorMsg = document.querySelector('#error-msg')
 
 const elResultTip = document.querySelector('.result_amt--tip')
 const elResultTotal = document.querySelector('.result_amt--total')
 
 const elBtnTip = document.querySelectorAll('.btn--tip')
 const elTipCustom = document.querySelector('#custom-tip')
+
+elErrorMsg.setAttribute('class', 'hidden');
 
 let bill = 0;
 let no_of_ppl = 0;
@@ -16,28 +19,39 @@ let tip_custom = 0;
 let total_tip = 0;
 let total_bill = 0;
 
-let tip_per_person = 0;
-let bill_per_person = 0;
+let tip_per_person = 0.00;
+let bill_per_person = 0.00;
 
-//Get total tip
+// Functions
 const calcTotalTip = (nbill, ntip) => nbill * (ntip/100);
 const calcTotalBill = (nbill, ntip) => nbill + ntip;
+const formatResultValue = (result) => Number.isInteger(result) ? `${result}.00` : result.toFixed(2);
+
 
 for (const btn of elBtnTip) {
   btn.addEventListener('click', function() {
     bill = Number(elBill.value)
     no_of_ppl = Number(elNoOfPeople.value)
+
     tip = Number(btn.getAttribute('data-tip-amount'))
     tip_custom = elTipCustom.value
 
-    total_tip = calcTotalTip(bill, tip);
-    total_bill = calcTotalBill(bill, total_tip);
+    if(no_of_ppl == 0) {
+      elErrorMsg.setAttribute('class', 'visible')
+      elNoOfPeople.style.border = `2px solid ${'hsl(var(--clr-red))'}`
+      
+    } else {
+      total_tip = calcTotalTip(bill, tip);
+      total_bill = calcTotalBill(bill, total_tip);
+  
+      tip_per_person = total_tip / no_of_ppl
+      bill_per_person = total_bill / no_of_ppl
+      elResultTip.innerHTML = `$${formatResultValue(tip_per_person)}`
+      elResultTotal.innerHTML = `$${formatResultValue(bill_per_person)}`
 
-    tip_per_person = total_tip / no_of_ppl
-    bill_per_person = total_bill / no_of_ppl
-
-    elResultTip.innerHTML = `$${tip_per_person.toFixed(2)}${Number.isInteger(tip_per_person) ? '.00' : ''}`
-    elResultTotal.innerHTML = `$${bill_per_person.toFixed(2)}${Number.isInteger(bill_per_person) ? '.00' : ''}`
+      elErrorMsg.setAttribute('class', 'hidden')
+      elNoOfPeople.style.border = '2px solid transparent'
+    }
 
     // console.log(total_tip, total_bill, tip_per_person, bill_per_person);
   })
