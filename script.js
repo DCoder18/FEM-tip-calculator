b882c9d1262eb8df66397670174ce15af671418e
Devika Sujith <devikasujith12@gmail.com>
2022-04-18 16:24:33 +0400

    Update script.js

--- script.js
+++ script.js
@@ -22,37 +22,66 @@ let total_bill = 0;
 let tip_per_person = 0.00;
 let bill_per_person = 0.00;
 
-// Functions
+// Utility Functions
 const calcTotalTip = (nbill, ntip) => nbill * (ntip/100);
 const calcTotalBill = (nbill, ntip) => nbill + ntip;
 const formatResultValue = (result) => Number.isInteger(result) ? `${result}.00` : result.toFixed(2);
+//
 
+const calculateSplitAmount = function() {
+  bill = Number(elBill.value)
+  no_of_ppl = Number(elNoOfPeople.value)
+
+  tip = Number(btn.getAttribute('data-tip-amount'))
+  
+  if(no_of_ppl == 0) {
+    elErrorMsg.setAttribute('class', 'visible')
+    elNoOfPeople.style.border = `2px solid ${'hsl(var(--clr-red))'}`
+  } else {
+    total_tip = calcTotalTip(bill, tip);
+    total_bill = calcTotalBill(bill, total_tip);
+    
+    tip_per_person = total_tip / no_of_ppl
+    bill_per_person = total_bill / no_of_ppl
+
+    elResultTip.innerHTML = `$${formatResultValue(tip_per_person)}`
+    elResultTotal.innerHTML = `$${formatResultValue(bill_per_person)}`
+
+    elErrorMsg.setAttribute('class', 'hidden')
+    elNoOfPeople.style.border = '2px solid transparent'
+}
 
 for (const btn of elBtnTip) {
-  btn.addEventListener('click', function() {
-    bill = Number(elBill.value)
-    no_of_ppl = Number(elNoOfPeople.value)
+  btn.addEventListener('click', calculateSplitAmount, false)
+}
 
-    tip = Number(btn.getAttribute('data-tip-amount'))
-    tip_custom = elTipCustom.value
 
-    if(no_of_ppl == 0) {
-      elErrorMsg.setAttribute('class', 'visible')
-      elNoOfPeople.style.border = `2px solid ${'hsl(var(--clr-red))'}`
+// for (const btn of elBtnTip) {
+//   btn.addEventListener('click', function() {
+//     bill = Number(elBill.value)
+//     no_of_ppl = Number(elNoOfPeople.value)
+
+//     tip = Number(btn.getAttribute('data-tip-amount'))
+//     tip_custom = getCustomTip()
+    
+
+//     if(no_of_ppl == 0) {
+//       elErrorMsg.setAttribute('class', 'visible')
+//       elNoOfPeople.style.border = `2px solid ${'hsl(var(--clr-red))'}`
       
-    } else {
-      total_tip = calcTotalTip(bill, tip);
-      total_bill = calcTotalBill(bill, total_tip);
+//     } else {
+//       total_tip = calcTotalTip(bill, tip);
+//       total_bill = calcTotalBill(bill, total_tip);
+      
   
-      tip_per_person = total_tip / no_of_ppl
-      bill_per_person = total_bill / no_of_ppl
-      elResultTip.innerHTML = `$${formatResultValue(tip_per_person)}`
-      elResultTotal.innerHTML = `$${formatResultValue(bill_per_person)}`
+//       tip_per_person = total_tip / no_of_ppl
+//       bill_per_person = total_bill / no_of_ppl
+//       elResultTip.innerHTML = `$${formatResultValue(tip_per_person)}`
+//       elResultTotal.innerHTML = `$${formatResultValue(bill_per_person)}`
 
-      elErrorMsg.setAttribute('class', 'hidden')
-      elNoOfPeople.style.border = '2px solid transparent'
-    }
-
-    // console.log(total_tip, total_bill, tip_per_person, bill_per_person);
-  })
+//       elErrorMsg.setAttribute('class', 'hidden')
+//       elNoOfPeople.style.border = '2px solid transparent'
+//     }
+//   })
+// }
 }
